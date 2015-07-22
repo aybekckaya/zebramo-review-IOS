0c7ee25a26d28cf871b935735223fcd96af83a75
aybek can kaya <aybekcankaya@aybeks-MacBook-Pro.local>
2015-07-22 12:32:53 +0300

    Initial Commit

---
+++ ZebramoReview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZebramoReview
+//
+//  Created by aybek can kaya on 22/07/15.
+//  Copyright (c) 2015 aybek can kaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
